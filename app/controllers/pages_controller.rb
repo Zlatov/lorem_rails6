c6c251d1b55e07ebb297e367c6e8675c853c4b98
zlatov <zlatov@ya.ru>
2020-07-18 23:56:42 +0300

    pages_controller.rb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+# Вывод главной страницы, статических страниц и динамических страниц Page
+class PagesController < ApplicationController
+
+  layout :calc_layout
+
+  # Вывод главной страницы
+  def home
+  end
+
+  def about
+  end
+
+  def page
+    @slug = params[:slug]
+  end
+
+  private
+
+  def calc_layout
+    case action_name
+    when 'home'
+      'home'
+    else
+      'page'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Главная страница
+  root "pages#home"
+  # Статические страниц
+  get   "about" => "pages#about"
+  match "pages/:slug" => "pages#page", as: "page", via: [:get, :post]
 end
